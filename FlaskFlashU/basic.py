349ef1e57b077868d9750e65e93591f514e9b4d3
Andrew Kiryukhin <andrewkir2001@mail.ru>
2018-10-16 17:16:59 +0300

    add FlaskFlash and virtual environment

---
+++ FlaskFlashU/basic.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, flash, session, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'somekey'
+
+
+class SimpleForm(FlaskForm):
+    name = StringField('What is your name', validators=[DataRequired()])
+    submit = SubmitField('Click me')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = SimpleForm()
+    if form.validate_on_submit():
+        session['name'] = form.name.data
+        flash('YO, {} click button tho'.format(session['name']))
+        return redirect(url_for('index'))
+
+    return render_template('index.html', form=form)
+
+
+if __name__ == '__main__':
+    app.run()
